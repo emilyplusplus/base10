95f16416e97b7c06b6c580756ad17c6bcd3feff3
Emily Carlsen <emilytcyt@gmail.com>
2017-08-26 11:36:48 -0400

    Hide dock icon, menu, fix win draw loc, hide on blur, global shortcut

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-const {app, BrowserWindow, Tray} = require('electron')
+const {app, BrowserWindow, Menu, globalShortcut, Tray} = require('electron')
+
+app.dock.hide()
 
 let tray = null
-let win = new BrowserWindow({width: 800, height: 600, frame: false})
+let win = null
 
 app.on('ready', () => {
     
-  tray = new Tray('calculator16.png')
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
+
+    tray = new Tray('calculator16.png')
+    let bounds = tray.getBounds()
+    console.log(bounds)
+
+    globalShortcut.register('CommandOrControl+Shift+B', () => {
+        win.isVisible() ? win.hide() : win.show()
+      })
+
+  win = new BrowserWindow({x: bounds.x - 100 + bounds.width / 2, y: bounds.y, width: 200, height: 300, frame: false, show: false, skipTaskbar: true, toolbar: false})
+    
+
+
+  win.setMenu(null)
+  Menu.setApplicationMenu(null)
+  
+
   tray.setToolTip('This is my application.')
-  tray.setContextMenu(contextMenu)
+
   tray.on('click', () => {
     win.isVisible() ? win.hide() : win.show()
   })
+})
+
+app.on('browser-window-blur', () => {
+    win.hide()
 })
